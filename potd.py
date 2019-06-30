0ce13d99f119e356f64d69948aa555d73153e42c
aw-west <39385377+aw-west@users.noreply.github.com>
2019-06-30 13:38:53 +0100

    1.5
    Reduce importing with `import from`
    Removed asserts and added try.  - maybe add error reasons.

--- potd.py
+++ potd.py
@@ -10,17 +10,16 @@
 #   is .jpg good enough
 #   installer creates shell:startup and sets desktop-wallpaper
 
-import requests
+from requests import get
 from bs4 import BeautifulSoup
-import time
+from time import strftime
 import os
 import ctypes
 
-def download(url, path):
-	print(f'\tdownloading: {url}\n\t\tinto: {path}')
-	r = requests.get(url)
-	assert(r.status_code is 200)
-	with open(path, 'wb') as f:
+def download(url, file):
+	print(f'\tdownloading: {url}\n\t\tinto: {file}')
+	r = get(url)
+	with open(file, 'wb') as f:
 		[f.write(chunk) for chunk in r]
 	return None
 
@@ -28,53 +27,42 @@ def get_url(id):
 	if id == 'bing':
 		print('Bing')
 		url = 'http://www.bing.com'
-		r = requests.get(url)
-		assert(r.status_code is 200)
+		r = get(url)
 		img_url = url+r.text.rsplit('g_img={url:',1)[1].split('};',1)[0].split('\\',1)[0].replace('"','').replace("'",'').replace(' ','')
-		assert(img_url is not None)
 	elif id == 'guardian':
 		print('Guardian')
 		url = 'http://www.theguardian.com/international'
-		r = requests.get(url)
-		assert(r.status_code is 200)
+		r = get(url)
 		soup = BeautifulSoup(r.content, 'lxml')
-		url = soup.find("div", {"data-id":"uk-alpha/special-other/special-story"}).find("a", {"class":"js-headline-text"})['href']
-		r = requests.get(url)
-		assert(r.status_code is 200)
+		url = soup.find('div', {'data-id':'uk-alpha/special-other/special-story'}).find('a', {'class':'js-headline-text'})['href']
+		r = get(url)
 		soup = BeautifulSoup(r.content, 'lxml')
-		img_url = soup.select("div.u-responsive-ratio")[0].find_all("source")[0]['srcset'].rsplit(',')[-1].strip().split(' ')[0]
-		assert(img_url is not None)
+		img_url = soup.select('div.u-responsive-ratio')[0].find_all('source')[0]['srcset'].rsplit(',')[-1].strip().split(' ')[0]
+		# img_url = soup.select("div.immersive-main-media.immersive-main-media__gallery")[0].find_all("source")[0]['srcset'].rsplit(',')[-1].strip().split(' ')[0]
 	elif id == 'nasa':
 		print('NASA')
 		url = 'http://apod.nasa.gov/'
-		r = requests.get(url)
-		assert(r.status_code is 200)
+		r = get(url)
 		soup = BeautifulSoup(r.content, 'lxml')
 		img_url = url+soup.find('img')['src']
 	elif id == 'ng':
 		print('National Geographic')
 		url = 'http://www.nationalgeographic.com/photography/photo-of-the-day/'
-		r = requests.get(url)
-		assert(r.status_code is 200)
+		r = get(url)
 		soup = BeautifulSoup(r.content, 'lxml')
 		img_url = soup.find('meta',{'property':'og:image'})['content']
-		assert(img_url is not None)
 	elif id == 'smith':
 		print('Smithsonian')
 		url = 'https://www.smithsonianmag.com/photocontest/photo-of-the-day/'
-		r = requests.get(url)
-		assert(r.status_code is 200)
+		r = get(url)
 		soup = BeautifulSoup(r.content, 'lxml')
 		img_url = 'https://'+soup.find('div',class_='photo-contest-detail-image').find("img")['src'].rsplit('https://',1)[1]
-		assert(img_url is not None)
 	elif id == 'wiki':
 		print('WikiMedia')
 		url = 'https://commons.wikimedia.org/wiki/Main_Page'
-		r = requests.get(url)
-		assert(r.status_code is 200)
+		r = get(url)
 		soup = BeautifulSoup(r.content, 'lxml')
 		img_url = soup.find('img')['src'].replace('thumb/','').rsplit('/',1)[0]
-		assert(img_url is not None)
 	else:
 		print(f'WarningWebID:  {id} not recognised.')
 	return img_url
@@ -99,16 +87,19 @@ def main(ids, save):
 	today, hist = path+'/today/', path+'/history/'
 	if not os.path.isdir(today): os.mkdir(today)
 	if not os.path.isdir(hist): os.mkdir(hist)
-	date = time.strftime('%Y-%m-%d ', time.gmtime())
+	date = strftime('%Y-%m-%d ')
 
 	listdir = os.listdir(today)
 	for id in ids:
 		listdir_id = list(filter(lambda el: id in el, listdir))
 		img_name = date+id+'.jpg'
 		if img_name not in listdir_id:
-			img_url = get_url(id)
-			download(img_url, today+img_name)
-			sorting(id, img_name, listdir_id, today, hist, save)
+			try:
+				img_url = get_url(id)
+				download(img_url, today+img_name)
+				sorting(id, img_name, listdir_id, today, hist, save)
+			except:
+				print('\tfailed')
 	set_wallpaper(today)
 	return None
 
